c9adf6e1b623f6b795aa7abfaa531809b1a1ea93
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-07-21 12:50:51 +0530

    Adding Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,58 @@ class App extends React.Component {
     const { posts } = this.props;
     return (
       <div>
+        <div className="nav">
+          <div className="left-div">
+            <img
+              src="https://image.flaticon.com/icons/svg/867/867644.svg"
+              alt="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://www.flaticon.com/premium-icon/icons/svg/2383/2383154.svg"
+              alt="search-icon"
+            />
+            <input placeholder="Search" />
+
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://www.flaticon.com/premium-icon/icons/svg/2667/2667380.svg"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://www.flaticon.com/premium-icon/icons/svg/2667/2667380.svg"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://www.flaticon.com/premium-icon/icons/svg/2667/2667380.svg"
+                alt="user-dp"
+                id="user-dp"
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log in</li>
+                <li>Log out</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </div>
         <PostsList posts={posts} />
       </div>
     );
