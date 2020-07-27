c02d06cf26758a0744520e1f1af46e117d04688d
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-07-27 10:29:48 +0530

    Logging user in - III

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,9 +7,22 @@ export function startLogin() {
     type: LOGIN_START,
   };
 }
+export function loginFailed(errorMessage) {
+  return {
+    type: LOGIN_FAILED,
+    error: errorMessage,
+  };
+}
+export function loginSuccess(user) {
+  return {
+    type: LOGIN_SUCCESS,
+    user,
+  };
+}
 
 export function login(email, password) {
   return (dispatch) => {
+    dispatch(startLogin());
     const url = APIUrls.login();
     fetch(url, {
       method: 'POST',
@@ -17,6 +30,16 @@ export function login(email, password) {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       body: getFormBody({ email, password }),
-    });
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data', data);
+        if (data.success) {
+          // dispatch action to save user
+          dispatch(loginSuccess(data.data.user));
+          return;
+        }
+        dispatch(loginFailed(data.message));
+      });
   };
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import { login } from '../actions/auth';
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -23,12 +25,20 @@ class Login extends Component {
     e.preventDefault();
     // console.log('this.emailInputRef', this.emailInputRef);
     // console.log('this.passwordInputRef', this.passwordInputRef);
+    console.log('this.state', this.state);
+    const { email, password } = this.state;
+
+    if (email && password) {
+      this.props.dispatch(login(email, password));
+    }
   };
 
   render() {
+    const { error, inProgress } = this.props.auth;
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
+        {error && <div className="alert error-dailog">{error}</div>}
         <div className="field">
           <input
             type="email"
@@ -50,11 +60,25 @@ class Login extends Component {
           />
         </div>
         <div className="field">
-          <button onClick={this.handleFormSubmit}>Log In</button>
+          {inProgress ? (
+            <button onClick={this.handleFormSubmit} disabled={inProgress}>
+              Logging in...
+            </button>
+          ) : (
+            <button onClick={this.handleFormSubmit} disabled={inProgress}>
+              Log In
+            </button>
+          )}
         </div>
       </form>
     );
   }
 }
 
-export default Login;
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+  };
+}
+
+export default connect(mapStateToProps)(Login);
